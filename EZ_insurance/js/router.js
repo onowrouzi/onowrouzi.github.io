d067b3d33727a881d0fc45ba23dab27866ec89cf
onowrouzi <onowrouzi@outlook.com>
2016-07-13 00:06:56 -0500

    Uploaded EZ insurance site.

---
+++ EZ_insurance/js/router.js
@@ -0,0 +1,46 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .config(function ($stateProvider, $urlRouterProvider) {
+            //
+            // For any unmatched url, redirect to /state1
+            $urlRouterProvider.otherwise("/");
+            //
+            // Now set up the states
+            $stateProvider
+                .state('home', {
+                    url: "/",
+                    templateUrl: "views/home.html"
+                })
+                .state('products', {
+                    url: "/products",
+                    templateUrl: "views/products.html"
+                })
+                .state('quotes', {
+                    url: "/quotes",
+                    templateUrl: "views/quotes.html"
+                })
+                .state('payment', {
+                    url: "/payment",
+                    templateUrl: "views/payment.html"
+                })
+                .state('contact', {
+                    url: "/contact",
+                    templateUrl: "views/contact.html"
+                })
+                .state('vehicles', {
+                    url: "/vehicles",
+                    templateUrl: "views/vehicles.html"
+                })
+                .state('commercial', {
+                    url: "/commercial",
+                    templateUrl: "views/commercial.html"
+                })
+                .state('life', {
+                    url: "/life",
+                    templateUrl: "views/life.html"
+                });
+        });
+})();
